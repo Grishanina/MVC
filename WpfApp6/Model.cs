260aacb79927ba1184799aa327d9e965e53985dc
ГришанинаКИ <@106-08>
2023-01-13 17:47:38 +0300

    Добавьте файлы проекта.

---
+++ WpfApp6/Model.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp6
+{
+    public static class Model
+    {// блок с данными
+        static int count = 0;
+        public static List<string> datalist = new List<string> { "Иванов", "Петров", "Сидоров" };
+        
+        // блок для обращения к текстовым полям
+        public static TextBlock tb;
+        public static TextBlock tbCombo;
+        
+        // блок с бизнес-логикой
+        public static int Count // свойство для отображения числа в TextBlock
+        {
+            get
+            {
+                return count;
+            }
+            set
+            {
+                count = value;
+                tb.Text = count.ToString();
+            }
+        }
+        public static int Combo  // свойтсво для отображения фамилии в Combobox
+        {
+            set
+            {
+                tbCombo.Text = datalist[value];
+            }
+        }
+    }
+}
